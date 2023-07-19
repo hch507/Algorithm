d0c494b91bba4801256d64b404d8d2430c6d3cb9
Han Chunghee <96819679+hch507@users.noreply.github.com>
2023-07-19 18:36:09 +0900

    [Bronze V] Title: A+B - 3, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -0,0 +1,15 @@
+import sys
+input= sys.stdin.readline
+
+t = int(input())
+num1 = []
+num2 = []
+for i in range(t):
+    n, m = map(int, input().split())
+    num1.append(n)
+    num2.append(m)
+
+for j in range(t):
+    print(num1[j]+num2[j])
+
+
