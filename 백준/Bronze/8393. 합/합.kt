19c5349543532cb04c94574f9faf448a6744d74b
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-01-29 12:28:43 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 13232 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.kt
@@ -0,0 +1,10 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val num = nextInt()
+    var sum = 0
+    for (i in 1..num) {
+        sum = sum+i
+    }
+    println(sum)
+}
