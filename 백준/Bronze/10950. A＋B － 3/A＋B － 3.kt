13ccd0005f24239eee54382f4eec82b22f09229a
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-01-29 12:26:13 +0900

    [Bronze V] Title: A+B - 3, Time: 140 ms, Memory: 13516 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.kt
@@ -0,0 +1,12 @@
+import java.util.Scanner
+
+fun main() = with(Scanner(System.`in`)) {
+    val num = nextInt()
+    
+    for (i in 1..num) {
+        var A = nextInt()
+        var B = nextInt()
+        
+        println("${A + B}")
+    }
+}
