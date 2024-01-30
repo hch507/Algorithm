1ccad68e015e56da337e6d3b70045a864e2c8c2b
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-01-30 09:54:20 +0900

    [Bronze V] Title: A+B - 7, Time: 148 ms, Memory: 13548 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,11 @@
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)){
+   val num = nextInt()
+   for(i in 1..num){
+       var a1 = nextInt()
+       var a2 = nextInt()
+
+       println("Case #${i}: ${a1+a2}")
+   }
+}
