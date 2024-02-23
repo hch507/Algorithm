085291461c253e90d6c7414364939dbbde07f05e
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-02-23 22:10:43 +0900

    [Bronze V] Title: X보다 작은 수, Time: 464 ms, Memory: 28296 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.kt
@@ -1,12 +1,18 @@
-import java.util.*
+import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)){
-   val size = nextInt()
-   val num = nextInt()
-   for(i in 1..size){
-       var num2 = nextInt()
-        if (num2 < num) {
-            print("$num2 ")
+    val arr = ArrayList<Int>()
+
+    val num= nextInt()
+    val num2 = nextInt()
+    for( i in 1.. num){
+        arr.add(nextInt())
+    }
+
+    for ( i in arr){
+        if(i<num2){
+            print("${i} ")
         }
-   }
+    }
 }
+
