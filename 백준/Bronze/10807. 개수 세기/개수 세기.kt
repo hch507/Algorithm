764f488d8d0e36518dc077a36005dc63e01f70c0
Han Chunghee <96819679+hch507@users.noreply.github.com>
2024-10-10 22:03:53 +0900

    [Bronze V] Title: 개수 세기, Time: 104 ms, Memory: 13416 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.kt
+++ 백준/Bronze/10807. 개수 세기/개수 세기.kt
@@ -1,15 +1,18 @@
-import java.util.*
+import java.util.Scanner
 
 fun main() = with(Scanner(System.`in`)){
-   val num = nextInt()
-   var array = ArrayList<Int>()
-   for(i in 1..num){
-       array.add(nextInt())
-   }
-   var num2 = nextInt()
-   var cnt = 0 
-   for( i in array){
-       if(i==num2) cnt+=1
-   }
-   println(cnt)
+    var a = nextInt()
+    var b : ArrayList<Int> = ArrayList()
+    var cnt = 0
+    for(i in 0 until a){
+        b.add(nextInt())
+    }
+    var c = nextInt()
+
+    for(i in b){
+        if(c==i){
+           cnt ++
+        }
+    }
+    println(cnt)
 }
