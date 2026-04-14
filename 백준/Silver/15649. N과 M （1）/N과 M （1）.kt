117d64728efeaeeb13f601a65fdd8499791e9711
Han Chunghee <96819679+hch507@users.noreply.github.com>
2026-04-14 16:50:37 +0900

    [Silver III] Title: N과 M (1), Time: 304 ms, Memory: 52584 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.kt
@@ -1,33 +1,40 @@
-import java.util.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 var n = 0
 var m = 0
-lateinit var chk: Array<Boolean>
-lateinit var rs : Stack<Int>
-fun main() = with(Scanner(System.`in`)) {
-    n = nextInt()
-    m = nextInt()
-    chk =Array(n+1){false}
-    rs = Stack()
+lateinit var result : IntArray
+val sb = StringBuilder()
+lateinit var visited : BooleanArray
 
-    recur(0)
+fun main(){
+
+    val br = BufferedReader(InputStreamReader(System.`in`))
+
+    val input = br.readLine().split(" ").map { it.toInt() }
+
+    n = input[0]
+    m = input[1]
+    result = IntArray(m)
+    visited = BooleanArray(n+1)
+
+    solution(0)
+    print(sb)
 }
 
-fun recur(num: Int) {
-    if (num == m) {
-        for (i in rs) {
-            print("${i} ")
-        }
-        println()
+fun solution(depth : Int){
+
+    if (depth==m){
+        sb.append(result.joinToString(" ")).append("\n")
         return
     }
-    for (i in 1 until n+1)
-        if (chk[i]==false){
-            chk[i] =true
-            rs.push(i)
-            recur(num+1)
-            chk[i]= false
-            rs.pop()
-        }
+    for (i in 1..n){
+        if (visited[i]) continue
 
+        visited[i] = true
+        result[depth] = i
+        solution(depth + 1)
+        visited[i]=false
+    }
 }
+
