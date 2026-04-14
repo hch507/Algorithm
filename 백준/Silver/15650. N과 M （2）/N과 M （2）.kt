4bbcf0070fbdd0b3fef106fb941f1616a256d6b8
Han Chunghee <96819679+hch507@users.noreply.github.com>
2026-04-14 17:01:34 +0900

    [Silver III] Title: N과 M (2), Time: 136 ms, Memory: 24072 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.kt
@@ -1,35 +1,32 @@
-import java.util.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 var n = 0
 var m = 0
-lateinit var chk: Array<Boolean>
-lateinit var rs : Stack<Int>
-fun main() = with(Scanner(System.`in`)) {
-    n = nextInt()
-    m = nextInt()
-    chk =Array(n+1){false}
-    rs = Stack()
+lateinit var result : IntArray
+val sb = StringBuilder()
+fun main(){
+    val br = BufferedReader(InputStreamReader(System.`in`))
 
-    recur(0,1)
+    val input = br.readLine().split(" ").map { it.toInt() }
+    n = input[0]
+    m= input[1]
+    result = IntArray(m)
+    solution(0,1)
+    
+    print(sb)
+
 }
 
-fun recur(num: Int, idx :Int) {
-    if (num == m){
-        for (i in rs){
-            print("${i} ")
-        }
-        println()
+fun solution(depth : Int, start : Int){
+    if (depth==m){
+        sb.append(result.joinToString(" ")).append("\n")
         return
     }
-    for (i in idx until n+1){
 
-        if (chk[i]==false){
-            chk[i]=true
-            rs.push(i)
-            recur(num+1, i)
-            chk[i]=false
-            rs.pop()
-        }
+    for (i in start..n){
+        result[depth] = i
+        solution(depth+1, i+1)
     }
 }
-
